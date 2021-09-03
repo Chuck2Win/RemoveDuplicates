fbd192feb8235700530a81a34f5cbd38701fe1b3
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-03 13:07:17 +0900

    Create deduplicates.py

---
+++ deduplicates.py
@@ -0,0 +1,14 @@
+# graph 방식으로 제거
+# 길이가 가장 긴 방식으로 제거
+
+def remove_duplicates(graph):
+    temp = list(graph.keys())
+    temp2 = temp.copy()
+    for i in temp:
+        for j in graph[i].keys():
+                try:
+                    temp2.remove(j)
+                except:
+                    continue
+    return temp2
+    
