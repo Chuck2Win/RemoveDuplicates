0eb7577c145a406401774e08b2fa4e5f4c506a14
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-09 13:36:00 +0900

    Update hashing_functions.py

--- LSH/hashing_functions.py
+++ LSH/hashing_functions.py
@@ -53,6 +53,76 @@ class MinHashing(object):
         for (a,b) in self.rand_hashes:
             result.append(self.minHashing(shingles, a, b))
         return result
-    def forward(self, text:str) -> List[int]:
-        shingles = self.getShingles(text, n_shingles=self.n_shingles)
-        return self.getSignature(shingles, self.rand_hashes)
+    
+    def forward(self, corpus:List[str]) -> List[int]:
+        Sig_Mat = []
+        for text in tqdm(corpus):
+            shingles = self.getShingles(text, n_shingles=self.n_shingles)
+            Sig_Mat.append(self.getSignature(shingles, self.rand_hashes))
+        return Sig_Mat
+    
+    def LSH(self, sig_mat, b, r):
+        result = []
+        for t in range(len(sig_mat)):
+            sig = sig_mat[t]
+            # band 별로 bucket을 따로 만듬.
+            buckets = dict()
+            for i in range(b):
+                # 세로로 비교 하는 것을 인지해라.
+                band = ''.join([str(sig[j]) for j in range(i * r, (i + 1) * r)]) # 이거 자체가 hashing 함수 
+                if buckets.get(band, -1)==-1:
+                    buckets[band]=set()
+                buckets[band].add(t)
+            x = list([list(i) for i in buckets.values() if len(list(i))>1])
+            result.append(x)
+        return result
+
+# 최적의 b,r 찾기 -> 틀린 것임.
+_integration_precision = 0.001
+def _integration(f, a, b):
+    p = _integration_precision
+    area = 0.0
+    x = a
+    while x < b:
+        area += f(x+0.5*p)*p
+        x += p
+    return area, None
+
+try:
+    from scipy.integrate import quad as integrate
+except ImportError:
+    # For when no scipy installed
+    integrate = _integration
+
+
+def _false_positive_probability(threshold, b, r):
+    _probability = lambda s : 1 - (1 - s**float(r))**float(b)
+    a, err = integrate(_probability, 0.0, threshold)
+    return a
+
+
+def _false_negative_probability(threshold, b, r):
+    _probability = lambda s : 1 - (1 - (1 - s**float(r))**float(b))
+    a, err = integrate(_probability, threshold, 1.0)
+    return a
+
+
+def _optimal_param(threshold, num_perm, false_positive_weight,
+        false_negative_weight):
+    '''
+    Compute the optimal `MinHashLSH` parameter that minimizes the weighted sum
+    of probabilities of false positive and false negative.
+    '''
+    min_error = float("inf")
+    opt = (0, 0)
+    for b in range(1, num_perm+1):
+        max_r = int(num_perm / b)
+        for r in range(1, max_r+1):
+            fp = _false_positive_probability(threshold, b, r)
+            fn = _false_negative_probability(threshold, b, r)
+            error = fp*false_positive_weight + fn*false_negative_weight
+            if error < min_error:
+                min_error = error
+                opt = (b, r)
+    return opt
+
