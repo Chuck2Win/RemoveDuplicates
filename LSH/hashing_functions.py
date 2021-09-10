0fa094252d6f63c1d8fd1f8dabc516be7bbf9044
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-10 13:22:05 +0900

    Update hashing_functions.py

--- LSH/hashing_functions.py
+++ LSH/hashing_functions.py
@@ -18,10 +18,11 @@ def polynomialHashing(x,a,b):
   return (a*x+b)%p
 
 class MinHashing(object):
-    def __init__(self, num_perm, n_shingles):
+    def __init__(self, num_perm, n_shingles, tokenizer):
         self.num_perm = num_perm
         self.rand_hashes = [[random.randint(0, 10 ** 9), random.randint(0, 10 ** 9)] for i in range(num_perm)]
         self.n_shingles = n_shingles
+        self.tokenizer = tokenizer
     def stringHashing(self, text:str)->int:
         result = 0
         p = 53 # character의 개수만큼
@@ -33,10 +34,16 @@ class MinHashing(object):
         p = 10 ** 9 + 7
         return (a * x + b) % p
     # shingles 만들기
-    def getShingles(self, text:str, n_shingles:int) -> List:
-        result = [text[i:i+n_shingles] for i in range(len(text)-n_shingles)]
+    def getShingles(self, text:str) -> List:
+        tokenized_text = tokenizer(text)
+        result = []
+        for i in range(len(text)-self.n_shingles):
+            t = tokenized_text[i:i+n_shingles]
+            if t:
+                result.append(' '.join(t))
+        
         return list(set(result))
-     # with out speed up
+    # with out speed up
     def minHashing(self, shingles, a, b):
         result = -1
         for shingle in shingles:
@@ -48,7 +55,7 @@ class MinHashing(object):
                 if result>h:
                     result = h
         return result
-    def getSignature(self,shingles, rand_hashes):
+    def getSignature(self,shingles):
         result = []
         for (a,b) in self.rand_hashes:
             result.append(self.minHashing(shingles, a, b))
@@ -57,10 +64,9 @@ class MinHashing(object):
     def forward(self, corpus:List[str]) -> List[int]:
         Sig_Mat = []
         for text in tqdm(corpus):
-            shingles = self.getShingles(text, n_shingles=self.n_shingles)
-            Sig_Mat.append(self.getSignature(shingles, self.rand_hashes))
+            shingles = self.getShingles(text)
+            Sig_Mat.append(self.getSignature(shingles))
         return Sig_Mat
-    
     def LSH(self, sig_mat, b, r):
         result = []
         for t in range(len(sig_mat)):
@@ -77,52 +83,7 @@ class MinHashing(object):
             result.append(x)
         return result
 
-# 최적의 b,r 찾기 -> 틀린 것임.
-_integration_precision = 0.001
-def _integration(f, a, b):
-    p = _integration_precision
-    area = 0.0
-    x = a
-    while x < b:
-        area += f(x+0.5*p)*p
-        x += p
-    return area, None
-
-try:
-    from scipy.integrate import quad as integrate
-except ImportError:
-    # For when no scipy installed
-    integrate = _integration
-
-
-def _false_positive_probability(threshold, b, r):
-    _probability = lambda s : 1 - (1 - s**float(r))**float(b)
-    a, err = integrate(_probability, 0.0, threshold)
-    return a
-
-
-def _false_negative_probability(threshold, b, r):
-    _probability = lambda s : 1 - (1 - (1 - s**float(r))**float(b))
-    a, err = integrate(_probability, threshold, 1.0)
-    return a
-
-
-def _optimal_param(threshold, num_perm, false_positive_weight,
-        false_negative_weight):
-    '''
-    Compute the optimal `MinHashLSH` parameter that minimizes the weighted sum
-    of probabilities of false positive and false negative.
-    '''
-    min_error = float("inf")
-    opt = (0, 0)
-    for b in range(1, num_perm+1):
-        max_r = int(num_perm / b)
-        for r in range(1, max_r+1):
-            fp = _false_positive_probability(threshold, b, r)
-            fn = _false_negative_probability(threshold, b, r)
-            error = fp*false_positive_weight + fn*false_negative_weight
-            if error < min_error:
-                min_error = error
-                opt = (b, r)
-    return opt
+def getShingles(tokenized_text:List[str], n_shingles:int) -> List:
+    result = [tokenized_text[i:i+n_shingles] for i in range(len(text)-n_shingles)]
+    return list(set(result))
 
